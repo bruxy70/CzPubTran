eee0a87d6f533a946aae9c0d396ba72d1e546d7e
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2019-09-06 11:36:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,33 +7,27 @@ PROJECT_NAME = 'CzPubTran'
 PROJECT_AUTHORS = "Václav Chaloupka"
 PROJECT_EMAILS = 'vasek.chaloupka@hotmail.com'
 PROJECT_URL = "https://github.com/bruxy70/CzPubTran"
-SHORT_DESCRIPTION = ''
-
-#try:
-#    DESCRIPTION = open(os.path.join(PROJECT_ROOT, "README.md")).read()
-#except IOError:
-#    DESCRIPTION = SHORT_DESCRIPTION
-
+SHORT_DESCRIPTION = 'Calling CHAPS REST API to get information about public transit in CZ'
 
 setup(
     name=PROJECT_NAME.lower(),
     version=REVISION,
     author=PROJECT_AUTHORS,
     author_email=PROJECT_EMAILS,
-    packages=['blink1', 'blink1_tests'],
+    packages=['czpubtran'],
     zip_safe=True,
     include_package_data=False,
-    install_requires=['hidapi>=0.7.99', 'click', 'webcolors'],
-    test_suite='nose.collector',
-    tests_require=['mock', 'nose', 'coverage'],
+    install_requires=[],
+    test_suite='',
+    tests_require=[],
     url=PROJECT_URL,
     description=SHORT_DESCRIPTION,
     long_description_markdown_filename='README.md',  # uses setuptools-markdown
     license='MIT',
     entry_points={
         'console_scripts': [
-            'blink1-flash = blink1.flash:flash',
-            'blink1-shine = blink1.shine:shine'
+            'czpubtran.find_connection = czpubtran.find_connection:find_connection',
+            'czpubtran.find_schedule_guid = czpubtran.find_schedule_guid:find_schedule_guid'
         ]
     },
     classifiers=[
